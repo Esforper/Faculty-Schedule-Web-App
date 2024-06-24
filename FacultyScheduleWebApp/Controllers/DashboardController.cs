335c23a4cec69f5c396b9148ab442c8146544881
Esforper <dolanbayomerfaruk@gmail.com>
2024-06-24 22:58:09 +0300

    first

---
+++ FacultyScheduleWebApp/Controllers/DashboardController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FacultyScheduleWebApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Database()
+        {
+            return View();
+        }
+
+        public IActionResult CreateSchedule()
+        {
+            return View();
+        }
+
+        public IActionResult Settings()
+        {
+            return View();
+        }
+    }
+}
---
+++ FacultyScheduleWebApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using FacultyScheduleWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FacultyScheduleWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+        }
+
+        public IActionResult Index()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                // Kullanıcı giriş yapmışsa DashboardController'a yönlendir
+                return RedirectToAction("Index", "Dashboard");
+            }
+            else
+            {
+                // Kullanıcı giriş yapmamışsa normal şekilde Index view'sını göster
+                return View();
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
