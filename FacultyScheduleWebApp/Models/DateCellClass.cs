dbb48c8483b6fe541163ecd914691d05287d14a3
Esforper <dolanbayomerfaruk@gmail.com>
2024-06-27 19:28:19 +0300

    DateCellClass açıklamaları eklendi

--- FacultyScheduleWebApp/Models/DateCellClass.cs
+++ FacultyScheduleWebApp/Models/DateCellClass.cs
@@ -3,11 +3,15 @@
     public class DateCellClass
     {
         public Guid Id { get; set; } // Primary Key
-
+        //Genel ID, muhtemelen kullanmak gerekmeyecek bile ama benzersizlik açısından önemli
 
         public Guid LessonAcademian { get; set; }
+        //Akademisyen takvimine buradan erişebilecek
         public Guid LessonClass { get; set; }
-        public Guid LessonName { get; set; }
+        //Derslikler takvimine bu Guidden erişecek
+        //Dersliklerin kodları da benzersizdir ama bu bizim üniversiteye özel olabilir o sebeple böyle durmalı.
+        public string LessonCode { get; set; }
+        //Her ders kodu benzersizdir, derse buradan erişilecek
 
         public int CellRow { get; set; }
         public int CellColumn { get; set; }
